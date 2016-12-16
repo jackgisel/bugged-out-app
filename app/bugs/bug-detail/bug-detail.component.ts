b0f6d9b9506785f7253b7f6ff5fddb79f8ebe3fa
John Ryan Gisel <johnryangisel@Johns-MacBook-Pro.local>
2016-12-15 16:47:29 -0800

    added enums for the dropdowns, and loading them for the component page

--- app/bugs/bug-detail/bug-detail.component.ts
+++ app/bugs/bug-detail/bug-detail.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 
 import { BugService } from '../service/bug.service'
 import { Bug } from '../model/bug';
+import { STATUS, SEVERITY } from '../../shared/constant/constants';
 
 import { forbiddenStringValidator } from '../../shared/validation/forbidden-string.validator';
 
@@ -15,11 +16,17 @@ import { forbiddenStringValidator } from '../../shared/validation/forbidden-stri
 export class BugDetailComponent implements OnInit {
     private modelId = "bugModal";
     private bugForm: FormGroup;
-    @Input() currentBug = new Bug(null, null, 1, 1, null, null, null, null, null);
+    private statuses = STATUS;
+    private severities = SEVERITY;
+    private statusArr:string[] = [];
+    private severityArr:string[] = [];
+    @Input() currentBug = new Bug(null, null, this.statuses.Logged, this.severities.Severe, null, null, null, null, null);
 
     constructor(private formB: FormBuilder, private bugService: BugService) { }
 
     ngOnInit() {
+        this.statusArr = Object.keys(this.statuses).filter(Number);
+        this.severityArr = Object.keys(this.severities).filter(Number);
         this.configureForm();
     }
 
@@ -67,11 +74,11 @@ export class BugDetailComponent implements OnInit {
     }
 
     freshForm() {
-        this.bugForm.reset({ status: 1, severity: 1 });
+        this.bugForm.reset({ status: this.statuses.Logged, severity: this.severities.Severe });
         this.cleanBug();
     }
 
     cleanBug() {
-        this.currentBug = new Bug(null, null, 1, 1, null, null, null, null, null);
+        this.currentBug = new Bug(null, null, this.statuses.Logged, this.severities.Severe, null, null, null, null, null);
     }
 }
